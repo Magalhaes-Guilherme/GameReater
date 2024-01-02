9b0aecb18d9251e3b43e6a70da3bc8e86fd2d45d
Guilherme Magalhães <91348994+Magalhaes-Guilherme@users.noreply.github.com>
2024-01-02 11:50:53 -0300

    Add files via upload

---
+++ src/views/cadastro/Cadastro.jsx
@@ -0,0 +1,83 @@
+import {Link} from "react-router-dom";
+import {useForm} from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { cadastro } from '../../services/userService';
+
+import * as yup from "yup";
+
+import './Cadastro.css';
+
+const schema = yup.object({
+    username: yup.string().required('Usuário obrigatório'),
+    email: yup.string().email('Email inválido').required('Email obrigatório'),
+    password: yup.string().min(5,'Senha com no mínimo 5 caracteres').required(),
+    passwordConf: yup.string().required('Confirme a senha').oneOf([yup.ref('password')], 'As senhas devem coincidir!'),
+}).required();
+
+function Cadastro(){
+
+    const form = useForm({
+        resolver: yupResolver(schema)
+    });
+
+    const { register, handleSubmit, formState } = form;
+
+    const {errors} = formState;
+
+    const submit = async (data) => {
+        try {
+            await cadastro(data.username, data.email, data.password);
+            alert('Usuário cadastrado com sucesso!');
+        } catch (error) {
+            console.error('Erro ao cadastrar usuário:', error);
+            alert('Erro ao cadastrar usuário. Por favor, tente novamente mais tarde.');
+        }
+    };
+
+    return(
+        <div className='cadastro'>
+            <form className='cadastroTela' onSubmit={handleSubmit(submit)} noValidate>
+                <h1>Cadastrar</h1>   
+
+                <label htmlFor="username">Usuário:</label>
+                <input
+                    type='text'
+                    id='username'
+                    placeholder="Usuário"
+                    {...register('username')}
+                />
+                {errors.username?.message}
+
+                <label htmlFor="email">Email:</label>
+                <input
+                    type='text'
+                    id='email'
+                    placeholder="Email"
+                    {...register('email')}
+                />
+                {errors.email?.message}
+
+                <label htmlFor='password'>Senha:</label>
+                <input
+                    type='password'
+                    id="password" 
+                    {...register('password')}
+                />
+                {errors.password?.message}
+
+                <label htmlFor='passwordConf'>Confirmar Senha:</label>
+                <input
+                    type='password'
+                    id="passwordConf" 
+                    {...register('passwordConf')}
+                />
+                {errors.passwordConf?.message}
+
+                <input type="submit" value="Cadastrar" className='submit'/>
+            </form>
+            <p>Já possui uma conta? <Link to='/login'>Entrar</Link></p>
+        </div>
+    )
+}
+
+export default Cadastro
